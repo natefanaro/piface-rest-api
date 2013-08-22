4b90cdc8e33a350fd66827c2267d46d205497f41
Nate Fanaro <natefanaro@gmail.com>
2013-08-22 04:33:05 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,85 @@
+piface api
+
+About
+=====
+This is a generic REST API that interfaces with a PiFace board attached to a RaspberryPi.
+
+Current limitation, only one board is supported at a time.
+
+Requirements
+====
+Hardware: Raspberry PI and PiFace module.
+PHP 5.4+
+Apache with mod_rewrite enabled
+PHP SPI extension: https://github.com/frak/php_spi
+
+Install
+=====
+# Clone the repo, install composer, install requirements from composer.json
+    git clone https://github.com/natefanaro/piface_rest.git
+	cd piface_rest
+    curl -sS https://getcomposer.org/installer | php
+	php composer.phar install
+# Point apache to piface_rest/public_html
+## Makke sure apache allows .htaccess files
+
+API Endpoints
+====
+
+GET /input/
+GET /led/
+GET /output/
+GET /relay/
+GET /switch/
+
+Returns the status of all devices in that category. The response object contains an indexed array. That is pin => state. 
+
+Ex
+    curl http://raspberrypi.local/api/output
+    {"status":"200","response":[1,0,0,0,0,0,0,0]}
+
+====
+
+GET /input/:input
+GET /led/:led
+GET /output/:output
+GET /relay/:relay
+GET /switch/:switch
+
+Returns the status of one device. The response object contains the value of the device (either 0 or 1). 
+
+Ex
+    curl http://raspberrypi.local/api/output/1
+    {"status":"200","response":1}
+
+====
+
+GET /led/:led/:value
+GET /output/:output/:value
+GET /relay/:relay/:value
+
+Sets the value of one device. Value can either be 0 or 1, The response object contains the new value of the device.
+
+Ex
+    curl http://raspberrypi.local/api/output/1/0
+    {"status":"200","response":0}
+    
+====
+
+GET /led/test/
+
+Blinks the leds on and off in a pattern.
+
+Status codes
+====
+
+Status codes mirror http status codes. 
+
+200: Success
+    {"status":"200","response":1}
+
+404: Unknown endpoint. This could mean a bad path or you accessed a device that does not exist. (ie: led 40)
+    {"status":"404","message":"Route not found"}
+
+500: Exception or other error. The code and message are returned.
+    {"status":"500","exception":{"code":8,"message":"Undefined offset: -1"}}
---
+++ public_html/index.php
@@ -0,0 +1,156 @@
+<?php
+require '../vendor/autoload.php';
+use Pkj\Raspberry\PiFace\PiFaceDigital;
+
+if (!class_exists('\Spi')) {
+	die ("Spi extension must be installed (https://github.com/frak/php_spi)");
+}
+
+$app = new \Slim\Slim(array(
+	'debug' => false
+));
+
+// Exception handler (make sure debug is false above)
+$app->error(function (\Exception $e) use ($app) {
+	print json_encode(array(
+		'status' => '500', 
+		'exception' => array(
+			'code' => $e->getCode(),
+			'message' => $e->getMessage()
+		)
+	));
+});
+
+$app->notFound(function () use ($app) {
+	print json_encode(array(
+		'status' => '404', 
+		'message' => 'Route not found'
+	));
+});
+
+$pi = PiFaceDigital::create();
+
+// Defining min/max values
+\Slim\Route::setDefaultConditions(array(
+    'input'  => '[0-' . (count($pi->getInputPins()) - 1) . ']',
+    'led'    => '[0-' . (count($pi->getLeds()) - 1) . ']',
+    'output' => '[0-' . (count($pi->getOutputPins()) - 1)  . ']',
+    'relay'  => '[0-' . (count($pi->getRelays()) - 1) . ']',
+    'switch' => '[0-' . (count($pi->getSwitches()) - 1) . ']',
+    'value'  => '[0-1]',
+));
+
+// Begin read only devices
+
+// Input
+$app->get('/input/', function () use ($app, $pi) {
+	responseSuccess(outputAll($pi->getInputPins()));
+});
+$app->get('/input/:input', function ($input) use ($app, $pi) {
+	responseSuccess($pi->getInputPins()[$input]->getValue());
+});
+
+// Switch
+$app->get('/switch/', function () use ($app, $pi) {
+	responseSuccess(outputAll($pi->getSwitches()));
+});
+$app->get('/switch/:switch', function ($switch) use ($app, $pi) {
+	responseSuccess($pi->getSwitches()[$switch]->getValue());
+});
+
+// Begin read/write devices
+
+// Output
+$app->get('/output/', function () use ($app, $pi) {
+	responseSuccess(outputAll($pi->getOutputPins()));
+});
+$app->get('/output/:output', function ($output) use ($app, $pi) {
+	responseSuccess($pi->getOutputPins()[$output]->getValue());
+});
+$app->get('/output/:output/:value', function ($output, $value) use ($app, $pi) {
+	$pi->getOutputPins()[$output]->setValue($value);
+	responseSuccess(intval($value));
+});
+
+// LED (not really sure if this is needed, since this mirrors output)
+$app->get('/led/', function () use ($app, $pi) {
+	responseSuccess(outputAll($pi->getLeds()));
+});
+$app->get('/led/:led', function ($led) use ($app, $pi) {
+	responseSuccess($pi->getLeds()[$led]->getValue());
+});
+$app->get('/led/:led/:value', function ($led, $value) use ($app, $pi) {
+	$pi->getLeds()[$led]->setValue($value);
+	responseSuccess(intval($value));
+});
+
+// Relay
+$app->get('/relay/', function () use ($app, $pi) {
+	responseSuccess(outputAll($pi->getRelays()));
+});
+$app->get('/relay/:relay', function ($relay) use ($app, $pi) {
+	responseSuccess($pi->getRelays()[$relay]->getValue());
+});
+$app->get('/relay/:relay/:value', function ($relay, $value) use ($app, $pi) {
+	$pi->getRelays()[$relay]->setValue($value);
+	responseSuccess(intval($value));
+});
+
+/** 
+ * This test will turn off all leds, then cycle through them 8 times.
+ */
+$app->get('/led/test/', function () use ($app, $pi) {
+
+	// turn all off
+	$i = 0;
+	while ($i < 8) {
+		$pi->getLeds()[$i]->setValue(0);
+		$i++;
+	}
+
+	$i = 0;
+	while ($i < 64) {
+		$led = ($i % 8);
+		$prev_led = $led;
+
+		$prev_led--;
+		if ($led == 0) {
+		#	$prev_led = 7;
+		}
+
+		$pi->getLeds()[$prev_led]->setValue(0);
+		$pi->getLeds()[$led]->setValue(1);
+		$i++;
+
+		usleep(50000);
+	}
+
+	$pi->getLeds()[7]->setValue(0);
+	responseSuccess(1);
+});
+
+$app->run();
+
+/** 
+ * Gets values of inputs/outputs/etc
+ *
+ * @param Device class object
+ * @return Array of values for given class of device
+ */
+function outputAll($items) {
+	$out = array();
+	foreach ($items as $pin) {
+		$out[] = $pin->getValue();
+	}
+	return $out;
+}
+
+/** 
+ * Prints a success response for the API
+ * @todo I am not a fan of this. Find a way using the Slim framework to handle this.
+ *
+ * @param mixed $response Response message
+ */
+function responseSuccess($response) {
+	print json_encode(array('status' => '200', 'response' => $response));
+}
